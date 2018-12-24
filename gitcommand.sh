0241b7d9f1ded867fba192ef8101963145e941c4
changenameYanjingzhu <yanjingzhu123@outlook.com>
2018-12-24 16:27:30 +0800

    ddd

--- gitcommand.sh
+++ gitcommand.sh
@@ -1,5 +1,11 @@
 git config --global user.email "yanjingzhu123@outlook.com"
+
 git config --global user.name "Yanjingzhu"
+
+git checkout master
+
 git add --all
+
 git commit -m "ddd"
-git push https://e6514578552897bf05da4d6ab5b982ea9b13be71@github.com/Yanjingzhu/YanGitRepo.git
+
+git push https://$(PAT)@github.com/Yanjingzhu/YanGitRepo.git
