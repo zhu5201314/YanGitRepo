aeaf4b7b74f5bfde7966dde75223460b5308a9bc
changenameYanjingzhu <yanjingzhu123@outlook.com>
2018-12-24 17:39:12 +0800

    Update testgit.bat

--- testgit.bat
+++ testgit.bat
@@ -4,4 +4,4 @@ git config --global user.name "Yanjingzhu"
 
 git add --all
 git commit -m "ddd"
-git push https://d31f9e1079d568efd6379bdba45bc927f2d02ad6@github.com/Yanjingzhu/YanGitRepo.git
+git push https://d31f9e1079d568efd6379bdba45bc927f2d02ad6@github.com/Yanjingzhu/YanGitRepo.git HEAD:origin/master
